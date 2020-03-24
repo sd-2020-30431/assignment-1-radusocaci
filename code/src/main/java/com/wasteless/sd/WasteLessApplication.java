cb6345e630965a7b50024544e741db8033503325
Socaci Radu Andrei <radusocaci@gmail.com>
2020-03-24 14:35:57 +0200

    Added base project files

---
+++ code/src/main/java/com/wasteless/sd/WasteLessApplication.java
@@ -0,0 +1,13 @@
+package com.wasteless.sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteLessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteLessApplication.class, args);
+	}
+
+}
